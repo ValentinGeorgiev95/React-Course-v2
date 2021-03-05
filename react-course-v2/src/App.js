25d8bbfd7bdd9afca5543a84ba9a7cc94c478e10
Valentin Georgiev <valentin.georgiev@flatrocktech.com>
2021-03-05 13:16:58 +0200

    wip react burger course => redux

--- react-course-v2/src/App.js
+++ react-course-v2/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -10,15 +10,13 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-				<BrowserRouter>
-					<Layout>
-						<Switch>
-							<Route path="/checkout" component={Checkout} />
-							<Route path="/orders" component={Orders} />
-							<Route path="/" exact component={BurgerBuilder} />
-						</Switch>
-					</Layout>
-				</BrowserRouter>
+				<Layout>
+					<Switch>
+						<Route path="/checkout" component={Checkout} />
+						<Route path="/orders" component={Orders} />
+						<Route path="/" exact component={BurgerBuilder} />
+					</Switch>
+				</Layout>
 			</div>
 		);
 	}
