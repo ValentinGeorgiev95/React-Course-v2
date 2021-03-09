63638942791d4aa8b23bf7737ba35f492b57af7a
Valentin Georgiev <valentin.georgiev@flatrocktech.com>
2021-03-09 22:23:37 +0200

    wip authentication with small bug

--- react-course-v2/src/components/UI/Modal/Modal.js
+++ react-course-v2/src/components/UI/Modal/Modal.js
@@ -5,7 +5,6 @@ import Auxiliate from '../../../hoc/Auxiliate/Auxiliate';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
-
     shouldComponentUpdate(nextProps, nextState) {
         return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
     }
